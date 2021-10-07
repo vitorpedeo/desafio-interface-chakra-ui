766c6cc9bdeee66222bc420cb3c1721a827fee4d
Vitor Pereira <vitorpereira@discente.ufg.br>
2021-10-07 19:56:30 -0300

    :tada: Init project with typescript configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>worldtrip</title>
+      </Head>
+
+      <h1>WorldTrip</h1>
+    </div>
+  )
+}
